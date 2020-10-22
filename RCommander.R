21f83efbcbf0a5c73530d6a97f2c26d3ba78ef9d
miralles21 <73282391+miralles21@users.noreply.github.com>
2020-10-22 11:43:25 +0200

    Archivos R
    PRÁCTICA BIOQUÍMICA

---
+++ RCommander.R
@@ -0,0 +1,14 @@
+
+Datos <- 
+  readXL("C:/Users/alexm/Desktop/trabajos uni/SEGUNDO DE BIOLOGIA/Bioquímica 1/PRÁCTICAS DE LAB/practic bqca.xlsx",
+   rownames=FALSE, header=TRUE, na="", sheet="Hoja3", stringsAsFactors=TRUE)
+
+RegModel.1 <- lm(Abs.400.nm~µmol.PNF, data=Datos)
+summary(RegModel.1)
+attach(Datos)
+xnueva<-seq(min(µmol.PNF),max(µmol.PNF),length.out=100)
+xnueva
+ynueva<-predict(RegModel.1,newdata=data.frame(µmol.PNF=xnueva))
+plot(Abs.400.nm~µmol.PNF,data=Datos,main="Recta patrón PNF");abline(RegModel.1,col="gold")
+
+
